98827d41c0554966d7c1ce7a070cc8f6a27ea697
Wenlu <wyan0068@student.monash.edu>
2025-08-22 22:00:31 +1000

    Update 3.1 Activities 2-4

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,6 +1,27 @@
+
 <template>
-    <div>
-        <h1>User Information Form / Credentials</h1>
+    <div class="form">
+    <h1>User Information Form / Credentials</h1>
+        <form>
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username"><br>
+            
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password"><br>
+            
+            <label for="isAustralian">Australian Resident?</label><br>
+            <input type="checkbox" id="isAustralian" name="isAustralian"><br>
+            
+            <label for="reason">Reason For Joining:</label><br>
+            <textarea id="reason" name="reason" rows="3"></textarea><br>
+            
+            <label for="gender">Gender</label><br>
+            <select id="gender">
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+                <option value="other">Other</option>
+            </select>
+        </form>
     </div>
 </template>
 
